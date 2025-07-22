a5bf13299fa736aba3826aaa54155b21748cbb50
Astra <141445781+astrafr@users.noreply.github.com>
2025-07-22 14:59:27 -0400

    Update submit.js

--- pages/api/submit.js
+++ pages/api/submit.js
@@ -2,9 +2,9 @@ import { applications } from "./create";
 
 export default function handler(req, res) {
   if (req.method === "POST") {
-    const { id, answers } = req.body;
+    const { id, username, answers } = req.body;
     if (applications[id]) {
-      applications[id].responses.push(answers);
+      applications[id].responses.push({ username, answers });
       res.status(200).json({ message: "Saved" });
     } else {
       res.status(404).json({ error: "Application not found" });
